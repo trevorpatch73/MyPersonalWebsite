c91a0b6aab2a15e610d15b9e850ae61bd58b06a9
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-10-05 17:21:51 -0400

    Update monolith.py

--- monolith.py
+++ monolith.py
@@ -1,4 +1,4 @@
-from flask import *
+from flask import Flask, render_template, request, url_for, redirect
 from email.mime.text import MIMEText
 import smtplib
 from email.message import EmailMessage
