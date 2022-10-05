246499d6f06cc13c48e356bd42a8196077a977fd
Trevor Patch <101590046+trevorpatch73@users.noreply.github.com>
2022-10-05 17:25:03 -0400

    Update monolith.py

--- monolith.py
+++ monolith.py
@@ -49,4 +49,4 @@ def sendemail():
     return redirect('/');
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=443)
+    app.run(host='0.0.0.0', port=443, ssl_context=('cert.pem', 'key.pem'))
